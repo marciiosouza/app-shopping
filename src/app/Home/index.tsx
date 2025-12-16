9d330c7db2b8902317d72832d2ae8dcd996f195f
Marcio Souza <marciioasouzza@gmail.com>
2025-12-16 09:40:23 -0300

    feat: initial commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,10 @@
+import { View, Image } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+    </View>
+  );
+}
